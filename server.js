c6321e707c123e587229c60e8ec9c44402990d64
Gabriel Campos <gabrielfeliphe25@gmail.com>
2022-12-13 13:29:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -41,19 +41,5 @@ async function requireAPI() {
 
 }
 
-function getTime() {
-      const d = new Date();
-      let h = addZero(d.getHours());
-      let m = addZero(d.getMinutes());
-      let s = addZero(d.getSeconds());
-      let time = h + ":" + m + ':'+ s
-      return time
-}
-
-function addZero(i) {
-    if (i < 10) {i = "0" + i}
-    return i;
-}
-
 requireAPI();
 
