f772d14aeea6fbf1fb72a00e8d4ade2c01990df9
Gabriel Campos <gabriel.campos@fulltechpro.com.br>
2022-12-15 13:24:35 -0300

    chore:  implementado express e filtro para retorno

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ require('dotenv').config()
 var axios = require('axios');
 var ApiSendEmail = require('./sendemail-api.js')
 
+const express = require('express')
+const app = express()
+
+app.get('/',async function(request, response){
+    returnoFunc = await requireAPI()
+    response.status(200).json(returnoFunc);
+})
+
+app.listen(3000)
+
 const interval = 3000;
 
 var functionRunCheck = false;
@@ -14,7 +24,7 @@ var config = {
     }
 };
 
-var busReq
+var sendMail = false;
 
 async function requireAPI() {
     while (true) {
@@ -23,7 +33,7 @@ async function requireAPI() {
             .then(response => {
                 console.log(response.data)
                 busReq = response.data
-                console.log(busReq[0].stop_order)
+                sendMail = filterReadyStop1ShapeId(busReq)
             })
             .catch(error => {
                 console.log(error)
@@ -31,15 +41,22 @@ async function requireAPI() {
         // espera o intervalo especificado antes de fazer a próxima requisição
         await new Promise(resolve => setTimeout(resolve, interval));
 
-        if (busReq[0].stop_order >= 2) {
+        if (sendMail == true) {
             console.log("ONIBUS SAIU DO TERMINAL")
             retorno = await ApiSendEmail.sendEmail()
             console.log(retorno)
+            return ("Onibus Saiu do Terminal")
             break;
         }
     }
 
 }
 
-requireAPI();
+function filterReadyStop1ShapeId(arr) {
+    return arr.some(function(item) {
+      return item.trip_status === "READY" && item.stop_order === 1 && item.shape_id.endsWith("0");
+    });
+  }
 
+
+  
